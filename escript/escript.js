977b1128023fd8575ae48b7a5dadf5a7d6eddf38
DiogoIV <diogorodriguesdasilva156@gmail.com>
2025-05-13 11:21:05 -0300

    att

--- escript/escript.js
+++ escript/escript.js
@@ -2,28 +2,58 @@ const numerocal = [...document.querySelectorAll('.butao')]
 const operadores = [...document.querySelectorAll('.butao_especial')]
 const display = document.getElementById('res_calculos')
 const tlimpar = document.getElementsByClassName("cl_butao")[0]
+const tigual = document.getElementById('tigual')
 
-let limpar = true
+let limpar = false
+let decimal = false
 
 numerocal.forEach((el) => {
     el.addEventListener('click', (event) => {
-        if(limpar) {
-            display.innerHTML = ""
-            limpar = false
+        limpar = false
+        
+        if (event.target.innerHTML == ",") {
+            if(!decimal) {
+                decimal = true
+                if(display.innerHTML =="0") {
+                    display.innerHTML = "0,"
+                } else {
+                    display.innerHTML+=event.target.innerHTML
+                }
+            }            
         } else {
-            display.innerHTML += el.textContent
+                if (display.innerHTML == "0") {
+                    display.innerHTML = ""
+                }
+                display.innerHTML+=event.target.innerHTML
         }
+        
         
     })
 })
 
 operadores.forEach((el) => {
     el.addEventListener('click', (event) => {
+        if(!limpar) {
+            limpar = true
+            if (display.innerHTML == "0") {
+                display.innerHTML = ""
+            }
+            if (display.innerHTML == "x") {
+                display.innerHTML += "*"
+            } else {
+                display.innerHTML += event.target.innerHTML
+            }
+        }   
         
-        display.innerHTML += event.target.innerHTML
     })
 })
 
 tlimpar.addEventListener('click', () => {
+    limpar = false
+    decimal = false
     display.innerHTML = '0'
+})
+
+tigual.addEventListener('click', ()=> {
+    
 })
